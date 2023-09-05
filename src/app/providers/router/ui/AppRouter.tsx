07115dcce598c980178c5e1601353077155d415a
goozebumpz <goozebumpz@MacBook-Pro-goozebumpz.local>
2023-09-05 15:47:50 +0300

    check husky

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,6 +2,8 @@ import React, { type JSX, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 
+const errr = ''
+
 const AppRouter = (): JSX.Element => (
   <Suspense fallback={<div>Loading...</div>}>
     <Routes>
